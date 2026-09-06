59b907acfdaf9f4b03d53d057e20f20933b0f28f
aaron02 <aaron.herrsche@hotmail.com>
2026-09-06 15:19:07 +0200

    Implement new Visibility System and Grid/Cell Handler

--- src/world/Server/Packets/Handlers/QuestHandler.cpp
+++ src/world/Server/Packets/Handlers/QuestHandler.cpp
@@ -682,11 +682,11 @@ void WorldSession::handleQuestgiverHelloOpcode(WorldPacket& recvPacket)
     if (!parsePacket(recvPacket, srlPacket))
         return;
 
-    if (const auto questGiver = _player->getWorldMap()->getCreature(srlPacket.questGiverGuid.getGuidLowPart()))
+    if (const auto questGiver = _player->getWorldMapCreature(srlPacket.questGiverGuid.getRawGuid()))
     {
         if (!questGiver->isQuestGiver())
         {
-            sLogger.debug("Creature with guid {} is not a questgiver.", srlPacket.questGiverGuid.getGuidLowPart());
+            sLogger.debug("Creature with guid {} is not a questgiver.", srlPacket.questGiverGuid.getCounter());
             return;
         }
 
@@ -694,7 +694,7 @@ void WorldSession::handleQuestgiverHelloOpcode(WorldPacket& recvPacket)
     }
     else
     {
-        sLogger.debug("Invalid questgiver guid {}.", srlPacket.questGiverGuid.getGuidLowPart());
+        sLogger.debug("Invalid questgiver guid {}.", srlPacket.questGiverGuid.getCounter());
     }
 }
 
@@ -711,7 +711,7 @@ void WorldSession::handleQuestgiverStatusQueryOpcode(WorldPacket& recvPacket)
 
     if (srlPacket.questGiverGuid.isUnit())
     {
-        Creature* quest_giver = _player->getWorldMap()->getCreature(srlPacket.questGiverGuid.getGuidLowPart());
+        Creature* quest_giver = _player->getWorldMapCreature(srlPacket.questGiverGuid.getRawGuid());
         if (quest_giver)
             qst_giver = quest_giver;
         else
@@ -733,7 +733,7 @@ void WorldSession::handleQuestgiverStatusQueryOpcode(WorldPacket& recvPacket)
     }
     else if (srlPacket.questGiverGuid.isGameObject())
     {
-        GameObject* quest_giver = _player->getWorldMap()->getGameObject(srlPacket.questGiverGuid.getGuidLowPart());
+        GameObject* quest_giver = _player->getWorldMapGameObject(srlPacket.questGiverGuid.getRawGuid());
         if (quest_giver)
             qst_giver = quest_giver;
         else
@@ -777,7 +777,7 @@ void WorldSession::handleQuestGiverQueryQuestOpcode(WorldPacket& recvPacket)
 
     if (srlPacket.guid.isUnit())
     {
-        Creature* quest_giver = _player->getWorldMap()->getCreature(srlPacket.guid.getGuidLowPart());
+        Creature* quest_giver = _player->getWorldMapCreature(srlPacket.guid.getRawGuid());
         if (quest_giver)
             qst_giver = quest_giver;
         else
@@ -790,7 +790,7 @@ void WorldSession::handleQuestGiverQueryQuestOpcode(WorldPacket& recvPacket)
     }
     else if (srlPacket.guid.isGameObject())
     {
-        GameObject* quest_giver = _player->getWorldMap()->getGameObject(srlPacket.guid.getGuidLowPart());
+        GameObject* quest_giver = _player->getWorldMapGameObject(srlPacket.guid.getRawGuid());
         if (quest_giver)
             qst_giver = quest_giver;
         else
@@ -918,7 +918,7 @@ void WorldSession::handleQuestgiverRequestRewardOpcode(WorldPacket& recvPacket)
 
     if (srlPacket.questgiverGuid.isUnit())
     {
-        Creature* quest_giver = _player->getWorldMap()->getCreature(srlPacket.questgiverGuid.getGuidLowPart());
+        Creature* quest_giver = _player->getWorldMapCreature(srlPacket.questgiverGuid.getRawGuid());
         if (quest_giver)
             qst_giver = quest_giver;
         else
@@ -941,7 +941,7 @@ void WorldSession::handleQuestgiverRequestRewardOpcode(WorldPacket& recvPacket)
     }
     else if (srlPacket.questgiverGuid.isGameObject())
     {
-        GameObject* quest_giver = _player->getWorldMap()->getGameObject(srlPacket.questgiverGuid.getGuidLowPart());
+        GameObject* quest_giver = _player->getWorldMapGameObject(srlPacket.questgiverGuid.getRawGuid());
         if (quest_giver)
             qst_giver = quest_giver;
         else
@@ -995,7 +995,7 @@ void WorldSession::handleQuestgiverCompleteQuestOpcode(WorldPacket& recvPacket)
 
     if (srlPacket.questgiverGuid.isUnit())
     {
-        Creature* quest_giver = _player->getWorldMap()->getCreature(srlPacket.questgiverGuid.getGuidLowPart());
+        Creature* quest_giver = _player->getWorldMapCreature(srlPacket.questgiverGuid.getRawGuid());
         if (quest_giver)
             qst_giver = quest_giver;
         else
@@ -1015,7 +1015,7 @@ void WorldSession::handleQuestgiverCompleteQuestOpcode(WorldPacket& recvPacket)
     }
     else if (srlPacket.questgiverGuid.isGameObject())
     {
-        GameObject* quest_giver = _player->getWorldMap()->getGameObject(srlPacket.questgiverGuid.getGuidLowPart());
+        GameObject* quest_giver = _player->getWorldMapGameObject(srlPacket.questgiverGuid.getRawGuid());
         if (quest_giver)
             qst_giver = quest_giver;
         else
@@ -1079,7 +1079,7 @@ void WorldSession::handleQuestgiverChooseRewardOpcode(WorldPacket& recvPacket)
 
     if (srlPacket.questgiverGuid.isUnit())
     {
-        Creature* quest_giver = _player->getWorldMap()->getCreature(srlPacket.questgiverGuid.getGuidLowPart());
+        Creature* quest_giver = _player->getWorldMapCreature(srlPacket.questgiverGuid.getRawGuid());
         if (quest_giver)
             qst_giver = quest_giver;
         else
@@ -1093,7 +1093,7 @@ void WorldSession::handleQuestgiverChooseRewardOpcode(WorldPacket& recvPacket)
     }
     else if (srlPacket.questgiverGuid.isGameObject())
     {
-        GameObject* quest_giver = _player->getWorldMap()->getGameObject(srlPacket.questgiverGuid.getGuidLowPart());
+        GameObject* quest_giver = _player->getWorldMapGameObject(srlPacket.questgiverGuid.getRawGuid());
         if (quest_giver)
             qst_giver = quest_giver;
         else
@@ -1195,7 +1195,7 @@ void WorldSession::handlePushQuestToPartyOpcode(WorldPacket& recvPacket)
                             response = QUEST_SHARE_MSG_BUSY;
                         }
                         
-                        if (response == QUEST_SHARE_MSG_SHARING_QUEST && !pPlayer->isVisibleObject(_player->getGuid()))
+                        if (response == QUEST_SHARE_MSG_SHARING_QUEST && !pPlayer->seesGuid(_player->GetNewGUID()))
                         {
                             response = QUEST_SHARE_MSG_BUSY;
                         }

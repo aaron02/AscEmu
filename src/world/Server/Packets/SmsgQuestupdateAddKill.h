a2ec0d6d367ea57cf0194407a44786fbca42f68b
Zyres <mr@eichbuehlerhof.de>
2026-09-11 21:25:20 +0200

    Update SmsgQuestupdateAddKill.h for Mop

--- src/world/Server/Packets/SmsgQuestupdateAddKill.h
+++ src/world/Server/Packets/SmsgQuestupdateAddKill.h
@@ -37,9 +37,48 @@ namespace AscEmu::Packets
 
         bool internalSerialise(WorldPacket& packet) override
         {
-            packet << questId << mobEntry << count << tCount << guid;
+            if (m_protocol.expansion <= WoW::Expansion::_Cata)
+            {
+                packet << questId << mobEntry << count << tCount << guid;
 
-            return true;
+                return true;
+            }
+            else if (m_protocol.isMop())
+            {
+                // objective type and object id must match the objective list sent in SMSG_QUEST_QUERY_RESPONSE
+                const int32_t signedEntry = static_cast<int32_t>(mobEntry);
+                const uint8_t objectiveType = signedEntry < 0 ? 2 : 0; // 2 = gameobject, 0 = npc
+                const uint32_t objectId = signedEntry < 0 ? static_cast<uint32_t>(-signedEntry) : mobEntry;
+
+                packet << uint16_t(count);
+                packet << uint8_t(objectiveType);
+                packet << questId;
+                packet << uint16_t(tCount);
+                packet << objectId;
+
+                packet.writeBit(guid[0]);
+                packet.writeBit(guid[4]);
+                packet.writeBit(guid[2]);
+                packet.writeBit(guid[6]);
+                packet.writeBit(guid[1]);
+                packet.writeBit(guid[5]);
+                packet.writeBit(guid[7]);
+                packet.writeBit(guid[3]);
+                packet.flushBits();
+
+                packet.writeByteSeq(guid[2]);
+                packet.writeByteSeq(guid[7]);
+                packet.writeByteSeq(guid[3]);
+                packet.writeByteSeq(guid[0]);
+                packet.writeByteSeq(guid[4]);
+                packet.writeByteSeq(guid[5]);
+                packet.writeByteSeq(guid[1]);
+                packet.writeByteSeq(guid[6]);
+
+                return true;
+            }
+
+            return false;
         }
 
         bool internalDeserialise(WorldPacket& /*packet*/) override { return false; }
